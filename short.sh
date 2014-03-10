140c40a7d6a742b03825d4afb95f1ab8685ab6c2
Chris Johnson <chris@cjohnson.io>
2014-03-10 11:18:21 -0700

    updated short

--- short.sh
+++ short.sh
@@ -4,9 +4,9 @@ alias goblog="cd ~/proj/goblog"
 alias not="cd ~/notes"
 alias dot='cd $DOTDIR'
 alias gow='cd ~/gowrk'
-alias goph='cd /Users/chris/ciqss/gowrk/src/github.com/c-johnson/gophurls/part1_app'
+alias goph='cd $GOPATH/src/github.com/c-johnson/gophurls/part1_app'
 alias gith='cd $GOPATH/src/$GITH'
 
 # Generated
-alias draf="cd /Users/chris/ciqss/gowrk/src/github.com/c-johnson/draft"
-alias jacht="cd /Users/chris/ciqss/gowrk/src/github.com/clayu/jacht"
+alias draf="cd $GOPATH/src/github.com/c-johnson/draft"
+alias jacht="cd $GOPATH/src/github.com/clayu/jacht"
